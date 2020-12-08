e49360f19314906a6a0c58cbc57bad75e1212af0
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-12-08 19:45:27 +0300

    fix: perem names

--- classwork/src/classwork_2/Triangle.java
+++ classwork/src/classwork_2/Triangle.java
@@ -1,28 +1,28 @@
 package classwork_2;
 
 public class Triangle extends Figure implements Areas {
-    private double base;
+    private double base1;
     private double height;
-    private double weight;
+    private double base3;
 
     public Triangle(double base, double height) {
-        this.base = base;
+        this.base1 = base;
         this.height = height;
     }
 
     public Triangle(double first, double second, double third) {
-        this.base = first;
+        this.base1 = first;
         this.height = second;
-        this.weight = third;
+        this.base3 = third;
     }
 
     @Override
     double perimeter() {
-        return base + height + weight;
+        return base1 + height + base3;
     }
 
     @Override
     public double square() {
-        return (this.base * this.height * 0.5);
+        return (this.base1 * this.height * 0.5);
     }
 }
